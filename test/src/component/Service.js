4d3b212fe7ed907748751b7ef7e8bb14def5294e
Akash Sharma <akysharma.193@gmail.com>
2021-02-11 14:56:49 +0530

    call java API

--- test/src/component/Service.js
+++ test/src/component/Service.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 class Service extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: null
+        }
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/home').then((resp) => {
+            resp.json().then((result) => {
+                console.warn(result)
+            })
+        })
+    }
+
     render() {
         return (
             <h2>Hello Service</h2>
